b31fb95e488bce30899e9ffb51f25f9fa54070ac
dikwella123 <dikwella.perez2@gmail.com>
2017-08-16 08:30:17 +0300

    first commit

---
+++ perezxproject1.py
@@ -0,0 +1,40 @@
+from flask import Flask,  render_template  
+app = Flask(__name__)
+
+ 
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/About')
+def About():
+    return render_template('About.html')
+@app.route('/Login')
+def Login():
+    return render_template('Login.html')
+@app.route('/create')
+def create():
+    return render_template('create.html')
+@app.route('/delete')
+def delete():
+    return render_template('delete.html')
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+@app.route('/products')
+def products():
+    return render_template('products.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
